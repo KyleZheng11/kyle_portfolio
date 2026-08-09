db0d16a908c2a2c53f2a6350f24993a9aca16c7e
KyleZheng11 <k1105yle@gmail.com>
2026-08-09 12:59:26 -0400

    added sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,15 +1,16 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../../assets/images/logo-s.png'
-import LogoSubtitle from '../../../assets/images/logo_sub.png'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
       <img src={LogoS} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="slobadon" />
+      <img className="sub-logo" src={LogoSubtitle} alt="Kyle" />
     </Link>
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
@@ -32,6 +33,26 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/kylezheng11/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/KyleZheng11"
+        >
+          <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
